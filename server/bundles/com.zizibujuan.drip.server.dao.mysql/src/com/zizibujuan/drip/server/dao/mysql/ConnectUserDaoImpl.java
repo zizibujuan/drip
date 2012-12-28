a097686f3528599d8a2bf774197271fe2bb9aa52
jzw <zhengwei.jin@gmail.com>
2012-12-28 10:17:28 +0800

    新加入ConnectUserDao接口和实现类，并注入服务

---
+++ server/bundles/com.zizibujuan.drip.server.dao.mysql/src/com/zizibujuan/drip/server/dao/mysql/ConnectUserDaoImpl.java
@@ -0,0 +1,13 @@
+package com.zizibujuan.drip.server.dao.mysql;
+
+import com.zizibujuan.drip.server.dao.ConnectUserDao;
+
+/**
+ * 通过第三方网站提供的connect功能，接入的用户。 数据访问实现类
+ * 
+ * @author jzw
+ * @since 0.0.1
+ */
+public class ConnectUserDaoImpl implements ConnectUserDao {
+
+}
---
+++ server/bundles/com.zizibujuan.drip.server.dao/src/com/zizibujuan/drip/server/dao/ConnectUserDao.java
@@ -0,0 +1,11 @@
+package com.zizibujuan.drip.server.dao;
+
+/**
+ * 通过第三方网站提供的connect功能，接入的用户。
+ * 数据访问接口
+ * @author jzw
+ * @since 0.0.1
+ */
+public interface ConnectUserDao {
+
+}
