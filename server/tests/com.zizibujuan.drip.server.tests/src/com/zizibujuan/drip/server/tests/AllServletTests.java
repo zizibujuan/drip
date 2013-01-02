96f6e781204e0d45e306f6aba53f01094bbfeb48
jzw <zhengwei.jin@gmail.com>
2013-01-02 16:07:55 +0800

    将测试用到的包放在一个feature中

--- server/tests/com.zizibujuan.drip.server.tests/src/com/zizibujuan/drip/server/tests/AllServletTests.java
+++ server/tests/com.zizibujuan.drip.server.tests/src/com/zizibujuan/drip/server/tests/AllServletTests.java
@@ -1,11 +1,16 @@
 package com.zizibujuan.drip.server.tests;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
 /**
  * Servlet测试套件
  * @author jzw
  * @since 0.0.1
  */
-@RunWith()
+@RunWith(Suite.class)
+@SuiteClasses({LoginServletTests.class})
 public class AllServletTests {
 
 }
---
+++ server/tests/com.zizibujuan.drip.server.tests/src/com/zizibujuan/drip/server/tests/LoginServletTests.java
@@ -0,0 +1,15 @@
+package com.zizibujuan.drip.server.tests;
+
+import junit.framework.Assert;
+
+/**
+ * 测试用户登录方法
+ * @author jzw
+ * @since 0.0.1
+ */
+public class LoginServletTests {
+
+	public void testXXX(){
+		Assert.assertTrue(true);
+	}
+}
