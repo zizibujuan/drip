b1e36691a9bffff673caf74cd263ea21f39b05da
jzw <zhengwei.jin@gmail.com>
2013-05-06 16:44:35 +0800

    在提示词条中添加希腊小写字母

--- client/bundles/com.zizibujuan.drip.client.editor/static/mathEditor/mathContentAssist.js
+++ client/bundles/com.zizibujuan.drip.client.editor/static/mathEditor/mathContentAssist.js
@@ -37,14 +37,14 @@ define([ "dojo/_base/array" ], function(array) {
 			freq: 0,
 			label: "乘号",
 			iconClass: "drip_equation_icon drip_multiplication"
-		},{
+		},/*{
 			input: "*",
 			map: "*",
 			nodeName: "text",
 			freq: 0,
 			label: "*",
 			iconClass: ""
-		},{
+		},*/{
 			// 支持输入平方立方，或直接输入数字
 			input: "^",
 			map: "^",
@@ -119,7 +119,187 @@ define([ "dojo/_base/array" ], function(array) {
 			freq: 0,
 			label: "cot",
 			iconClass: ""
-		} ],
+		},
+		// 希腊字母,小写
+		{
+			input: "alpha",
+			map: "&#x3B1;",
+			nodeName: "mi",
+			freq: 0,
+			label: "阿尔法",
+			iconClass: "drip_equation_icon drip_alpha"
+		},{
+			input: "beta",
+			map: "&#x3B2;",
+			nodeName: "mi",
+			freq: 0,
+			label: "贝塔",
+			iconClass: "drip_equation_icon drip_beta"
+		},{
+			input: "gamma",
+			map: "&#x3B3;",
+			nodeName: "mi",
+			freq: 0,
+			label: "伽玛",
+			iconClass: "drip_equation_icon drip_gamma"
+		},{
+			input: "delta",
+			map: "&#x3B4;",
+			nodeName: "mi",
+			freq: 0,
+			label: "德尔塔",
+			iconClass: "drip_equation_icon drip_delta"
+		},{
+			input: "epsilon",
+			map: "&#x3B5;",
+			nodeName: "mi",
+			freq: 0,
+			label: "艾普西龙",
+			iconClass: "drip_equation_icon drip_epsilon"
+		},{
+			input: "zeta",
+			map: "&#x3B6;",
+			nodeName: "mi",
+			freq: 0,
+			label: "捷塔",
+			iconClass: "drip_equation_icon drip_zeta"
+		},{
+			input: "eta",
+			map: "&#x3B7;",
+			nodeName: "mi",
+			freq: 0,
+			label: "依塔",
+			iconClass: "drip_equation_icon drip_eta"
+		},{
+			input: "theta",
+			map: "&#x3B8;",
+			nodeName: "mi",
+			freq: 0,
+			label: "西塔",
+			iconClass: "drip_equation_icon drip_theta"
+		},{
+			input: "iota",
+			map: "&#x3B9;",
+			nodeName: "mi",
+			freq: 0,
+			label: "艾欧塔",
+			iconClass: "drip_equation_icon drip_iota"
+		},{
+			input: "kappa",
+			map: "&#x3BA;",
+			nodeName: "mi",
+			freq: 0,
+			label: "喀帕",
+			iconClass: "drip_equation_icon drip_kappa"
+		},{
+			input: "lambda",
+			map: "&#x3BB;",
+			nodeName: "mi",
+			freq: 0,
+			label: "拉姆达",
+			iconClass: "drip_equation_icon drip_lambda"
+		},{
+			input: "mu",
+			map: "&#x3BC;",
+			nodeName: "mi",
+			freq: 0,
+			label: "缪",
+			iconClass: "drip_equation_icon drip_mu"
+		},{
+			input: "nu",
+			map: "&#x3BD;",
+			nodeName: "mi",
+			freq: 0,
+			label: "拗",
+			iconClass: "drip_equation_icon drip_nu"
+		},{
+			input: "xi",
+			map: "&#x3BE;",
+			nodeName: "mi",
+			freq: 0,
+			label: "克西",
+			iconClass: "drip_equation_icon drip_xi"
+		},{
+			input: "omicron",
+			map: "&#x3BF;",
+			nodeName: "mi",
+			freq: 0,
+			label: "欧麦克轮",
+			iconClass: "drip_equation_icon drip_omicron"
+		},{
+			input: "pi",
+			map: "&#x3C0;",
+			nodeName: "mi",
+			freq: 0,
+			label: "派",
+			iconClass: "drip_equation_icon drip_pi"
+		},{
+			input: "rho",
+			map: "&#x3C1;",
+			nodeName: "mi",
+			freq: 0,
+			label: "柔",
+			iconClass: "drip_equation_icon drip_rho"
+		},{
+			input: "sigma",
+			map: "&#x3C3;",
+			nodeName: "mi",
+			freq: 0,
+			label: "西格玛",
+			iconClass: "drip_equation_icon drip_sigma"
+		},{
+			input: "tau",
+			map: "&#x3C4;",
+			nodeName: "mi",
+			freq: 0,
+			label: "套",
+			iconClass: "drip_equation_icon drip_tau"
+		},{
+			input: "upsilon",
+			map: "&#x3C5;",
+			nodeName: "mi",
+			freq: 0,
+			label: "宇普西龙",
+			iconClass: "drip_equation_icon drip_upsilon"
+		},{
+			input: "phi",
+			map: "&#x3C6;",
+			nodeName: "mi",
+			freq: 0,
+			label: "服艾",
+			iconClass: "drip_equation_icon drip_phi"
+		},{
+			input: "chi",
+			map: "&#x3C7;",
+			nodeName: "mi",
+			freq: 0,
+			label: "器",
+			iconClass: "drip_equation_icon drip_chi"
+		},{
+			input: "psi",
+			map: "&#x3C8;",
+			nodeName: "mi",
+			freq: 0,
+			label: "普赛",
+			iconClass: "drip_equation_icon drip_psi"
+		},{
+			input: "omega",
+			map: "&#x3C9;",
+			nodeName: "mi",
+			freq: 0,
+			label: "欧米伽",
+			iconClass: "drip_equation_icon drip_omega"
+		},
+		// 希腊字母,大写
+		{
+			input: "delta",
+			map: "&#x394;",
+			nodeName: "mi",
+			freq: 0,
+			label: "德尔塔(大写)",
+			iconClass: "drip_equation_icon drip_delta_upper"
+		}
+		],
 
 		getProposals : function(prefix) {
 			// summary:
