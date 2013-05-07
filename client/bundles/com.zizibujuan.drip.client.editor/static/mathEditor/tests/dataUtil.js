461080f20809ee669fdadbbb4c17f224bda2944d
jzw <zhengwei.jin@gmail.com>
2013-05-07 17:59:22 +0800

    调整了xml转换html逻辑后，调整测试用例代码

--- client/bundles/com.zizibujuan.drip.client.editor/static/mathEditor/tests/dataUtil.js
+++ client/bundles/com.zizibujuan.drip.client.editor/static/mathEditor/tests/dataUtil.js
@@ -2,12 +2,12 @@ define([ "doh","dojox/xml/parser","mathEditor/dataUtil" ], function(doh,domParse
 	doh.register("dataUtil",[
 		function testXmlStringToHtml(t){
 			var xmlString = "<root><line><text>a</text><math><mn>1</mn></math></line></root>";
-			var expectHtml = "<div class='drip_line'><span>a</span><math><mn>1</mn></math></div>";
+			var expectHtml = "<div class='drip_line'><span>a</span><span class=\"drip_math\"><math><mn>1</mn></math></span></div>";
 			t.is(expectHtml, dataUtil.xmlStringToHtml(xmlString));
 		},
 		function testUnicode(t){
 			var xmlString = "<root><line><math><mo>&#xF7;</mo></math></line></root>";
-			var expectHtml = "<div class='drip_line'><math><mo>÷</mo></math></div>";
+			var expectHtml = "<div class='drip_line'><span class=\"drip_math\"><math><mo>÷</mo></math></span></div>";
 			t.is(expectHtml, dataUtil.xmlStringToHtml(xmlString));
 		}
 	]);
