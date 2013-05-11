80afc21e0284c81ef0a52fe14a9eae5c41671e3c
jzw <zhengwei.jin@gmail.com>
2013-05-11 09:48:29 +0800

    添加在mn中间插入分数的测试用例，并删除无用的测试用例

--- client/bundles/com.zizibujuan.drip.client.editor/static/mathEditor/tests/model/setData/frac.js
+++ client/bundles/com.zizibujuan.drip.client.editor/static/mathEditor/tests/model/setData/frac.js
@@ -255,6 +255,42 @@ define([ "doh", "mathEditor/tests/testUtil", "mathEditor/Model" ], function(doh,
 				
 			}
 		},{
+			name: "在mn节点中输入mfrac",
+			setUp: function(){
+				this.model = new Model({});
+			},
+			runTest: function(t){
+				var model = this.model;
+				model.loadData("<root><line>" +
+						"<math>" +
+						"<mn>123</mn>" +
+						"</math>" +
+						"</line></root>");
+				model.mode = "mathml";
+				var line = model.getLineAt(0);
+				model.anchor.node = line.firstChild.firstChild;
+				model.anchor.offset = 1;
+				model.path = [];
+				model.path.push({nodeName: "root"});
+				model.path.push({nodeName: "line", offset: 1});
+				model.path.push({nodeName: "math", offset: 1});
+				model.path.push({nodeName: "mn", offset: 1});
+				model.setData({data:"", nodeName:"mfrac"});
+				//创建完成后，让分子先获取焦点
+				t.is("/root/line[1]/math[1]/mfrac[2]/mrow[1]/mn[1]", model.getPath()); 
+				var node = model.getFocusNode();
+				// 确保选中的是分子节点
+				t.t(node.parentNode.previousSibling == null);
+				testUtil.isPlaceHolder(t, model.anchor);
+				// 判断新的mfrac是添加在mstyle之后，而不是mstyle中
+				t.is(3, line.firstChild.childElementCount);
+				// 确定新插入的mfrac在layout前
+				t.is(node.parentNode.parentNode.parentNode, line.firstChild.firstChild.nextSibling);
+			},
+			tearDown: function(){
+				
+			}
+		},{
   			name: "在一个已输入中文的model中加入一个空的分数",
   			setUp: function(){
   				this.model = new Model({});
@@ -302,45 +338,6 @@ define([ "doh", "mathEditor/tests/testUtil", "mathEditor/Model" ], function(doh,
   				
   				t.is("你", children[0].textContent);
   				t.is("我", children[2].textContent);
-  			},
-  			tearDown: function(){
-  				
-  			}
-  		},{
-  			name: "在包含一个数字的编辑器中输入分数，之前的数字变为分子，分母获取焦点",
-  			setUp: function(){
-  				this.model = new Model({});
-  			},
-  			runTest: function(t){
-  				/**
-  				 * <pre>
-  				 * FROM
-  				 * <math>
-  				 * 	<mn>1</mn>
-  				 * </math>
-  				 *   TO
-  				 * <math>
-  				 * 	<mfrac>
-  				 *    <mrow><mn>1</mn></mrow>  分子
-  				 *    <mrow><mn></mn></mrow>   分母
-  				 *  <mfrac>
-  				 * </math>
-  				 * </pre>
-  				 */
-  				var model = this.model;
-  				model.toMathMLMode();
-  				model.setData({data:"1"});
-  				model.setData({data:"", nodeName:"mfrac"});
-  				t.is("/root/line[1]/math[1]/mfrac[1]/mrow[2]/mn[1]", model.getPath()); //创建完成后，让分母先获取焦点
-  				var node = model.getFocusNode();
-  				t.is("mn", node.nodeName);
-  				t.is("drip_placeholder_box", node.getAttribute("class"));
-  				t.is(0, model.getOffset());
-  				
-  				// 判断分子的值为1
-  				var numeratorNode = node.parentNode.previousSibling.firstChild;
-  				t.is("mn", numeratorNode.nodeName);
-  				t.is("1", numeratorNode.textContent);
   			},
   			tearDown: function(){
   				
