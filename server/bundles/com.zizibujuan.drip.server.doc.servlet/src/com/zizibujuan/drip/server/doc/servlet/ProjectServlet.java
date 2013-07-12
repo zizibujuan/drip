1115f5e88813a566ad116d957151dc5a45d487f2
jzw <zhengwei.jin@gmail.com>
2013-07-12 17:17:07 +0800

    创建doc的服务层项目，并在启动脚本中引入新建的项目。

--- server/bundles/com.zizibujuan.drip.server.doc.servlet/src/com/zizibujuan/drip/server/doc/servlet/ProjectServlet.java
+++ server/bundles/com.zizibujuan.drip.server.doc.servlet/src/com/zizibujuan/drip/server/doc/servlet/ProjectServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+
 import com.zizibujuan.drip.server.util.servlet.BaseServlet;
 
 /**
@@ -18,12 +21,19 @@ public class ProjectServlet extends BaseServlet {
 	private static final long serialVersionUID = -2474647543970725994L;
 
 	/**
-	 * 新建项目
+	 * 新建项目,并存放在git仓库中
 	 */
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		traceRequest(req);
+		String pathInfo = req.getPathInfo();
+		IPath path = (pathInfo == null ? Path.ROOT : new Path(pathInfo));
+		if(path.segmentCount() == 0){
+			// TODO:需要添加一个service层
+			// TODO:获取存放所有仓库的根目录（在系统参数中配置，该版本使用cm服务）
+			return;
+		}
 		super.doPost(req, resp);
 	}
 
