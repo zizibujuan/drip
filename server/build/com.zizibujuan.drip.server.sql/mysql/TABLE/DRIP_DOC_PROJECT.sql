557b446a76b93190d8f871481a6eca4fd9176971
jzw <zhengwei.jin@gmail.com>
2013-07-16 21:52:13 +0800

    创建存储项目信息的表

---
+++ server/build/com.zizibujuan.drip.server.sql/mysql/TABLE/DRIP_DOC_PROJECT.sql
@@ -0,0 +1,15 @@
+-- -----------------------------------------------------
+-- Table `drip`.`DRIP_DOC_PROJECT` 维护项目信息
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `drip`.`DRIP_DOC_PROJECT`;
+
+CREATE  TABLE IF NOT EXISTS `drip`.`DRIP_DOC_PROJECT` (
+  `DBID` BIGINT NOT NULL AUTO_INCREMENT COMMENT '主键' ,
+  `PROJECT_NAME` VARCHAR(32) NULL COMMENT '项目名称' ,
+  `PROJECT_LABEL` VARCHAR(56) NULL COMMENT '项目中文名' ,
+  `PROJECT_DESC` VARCHAR(512) NULL COMMENT '项目详细描述',
+  `CRT_TM` DATETIME NULL COMMENT '创建时间' ,
+  `CRT_USER_ID` BIGINT NOT NULL COMMENT '创建人标识',
+  PRIMARY KEY (`DBID`))
+ENGINE = InnoDB
+COMMENT = '项目信息';
