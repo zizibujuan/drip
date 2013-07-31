2306e7562b79151716f71bdadf95474dfafc4756
jzw <zhengwei.jin@gmail.com>
2013-07-31 23:13:46 +0800

    初步显示项目根目录下的文件列表

--- client/bundles/com.zizibujuan.drip.client.doc/web/doc/projects/FileList.js
+++ client/bundles/com.zizibujuan.drip.client.doc/web/doc/projects/FileList.js
@@ -1,15 +1,27 @@
 define(["dojo/_base/declare",
+        "dojo/_base/lang",
+        "dojo/_base/array",
+        "dojo/date/locale",
+        "dojo/date/stamp",
+        "dojo/dom-construct",
         "dijit/_WidgetBase",
-        "drip/_StoreMixin"], function(
+        "drip/_StoreMixin",
+        "drip/prettyDate"], function(
 		declare,
+		lang,
+		array,
+		locale,
+		stamp,
+		domConstruct,
 		_WidgetBase,
-		_StoreMixin){
+		_StoreMixin,
+		prettyDate){
 	
 	return declare("projects.FileList", [_WidgetBase, _StoreMixin], {
 		
 		postCreate: function(){
 			this.inherited(arguments);
-			
+			this.refresh();
 		},
 		
 		refresh: function(){
@@ -27,11 +39,42 @@ define(["dojo/_base/declare",
 				 this.domNode.innerHTML = "";
 				 
 				 // 创建一个三列的table
+				 var table = domConstruct.create("table",{"class": "files"}, this.domNode);
+				 var tbody = domConstruct.create("tbody");
+				 table.appendChild(tbody);
+				 
 				 array.forEach(items, lang.hitch(this,function(item, index){
-					 console.log("file item", item);
+					 var row = domConstruct.create("tr", {}, tbody);
+					 
+					 var iconCell = domConstruct.create("td", {"class":"icon"}, row);
+					 var iconHtml = "";
+					 if(item.directory){
+						 iconHtml = "<i class=\"icon-folder-close-alt\"></i>";
+					 }else{
+						 iconHtml = "<i class=\"icon-file-text-alt\"></i>";
+					 }
+					 var iconContent = domConstruct.place(iconHtml, iconCell);
+					 
+					 var contentCell = domConstruct.create("td", {"class": "content"}, row);
+					 var contentSpan = domConstruct.create("span", {"class": ""}, contentCell);
+					 var contentLink = domConstruct.create("a", {"href": "#", "innerHTML": item.name}, contentSpan);
+					 
+					 var messageCell = domConstruct.create("td", {"class": "message"}, row);
+					 var messageSpan = domConstruct.create("span", {"class": ""}, messageCell);
+					 var messageLink = domConstruct.create("a", {"href": "#"}, messageSpan);
+					 debugger;
+					 var date = stamp.toISOString(new Date(item.lastModified));
+					 var ageCell = domConstruct.create("td", {"class": "age"}, row);
+					 var ageSpan = domConstruct.create("span", {"class": ""}, ageCell);
+					 var ageTime = domConstruct.create("time", {
+						 "datetime":date, 
+						 "innerHTML": prettyDate.pretty(date),
+						 "title": date.replace("T", " ")
+					}, ageSpan);
 				 }));
 			 }
 		}
 	
 	});
 });
+// 启动时间计时器
