10045cd7b25637060872a16130b64676d2117d39
jzw <zhengwei.jin@gmail.com>
2013-09-15 00:34:13 +0800

    将intern的目录结构调整为 static 	/node_modules 		/intern 	/dojo 	/mathEditor 并预先将dojo的代码复制一份，因为dojo的代码基本是稳定的，不需要每次运行测试 的时候都拷贝一份。等升级dojo的时候拷贝一次dojo。

--- client/bundles/com.zizibujuan.drip.client.editor/static/mathEditor/tests/intern.js
+++ client/bundles/com.zizibujuan.drip.client.editor/static/mathEditor/tests/intern.js
@@ -71,7 +71,7 @@ define({
 	suites: [  ],
 
 	// Functional test suite(s) to run in each browser once non-functional tests are completed
-	functionalSuites: [ "static/mathEditor/tests/functional/TextInput" ],
+	functionalSuites: [ "mathEditor/tests/functional/TextInput" ],
 
 	// A regular expression matching URLs to files that should not be included in code coverage analysis
 	excludeInstrumentation: /^tests\//
--- client/bundles/com.zizibujuan.drip.client.intern/static/runTest.sh
+++ client/bundles/com.zizibujuan.drip.client.intern/static/runTest.sh
@@ -4,14 +4,14 @@ BASEDIR=$(cd $(dirname $0) && pwd)
 DEVDIR="/home/jzw/git/private/aliyun/com.zizibujuan.drip/client/bundles/"
 SRCDIR="$BASEDIR"
 
-internDir=$baseDir/static/intern
-
 # copy files
 echo copy files from $DEVDIR/com.zizibujuan.drip.client.editor/static/mathEditor to $SRCDIR
 cp -r $DEVDIR/com.zizibujuan.drip.client.editor/static/mathEditor $SRCDIR
 
 # run
-node $SRCDIR/intern/runner.js config=static/mathEditor/tests/intern 
+pushd $SRCDIR/node_modules/intern
+node runner.js config=mathEditor/tests/intern reporters=console reporters=lcov
+popd
 
 #reporters=console reporters=lcov
 
