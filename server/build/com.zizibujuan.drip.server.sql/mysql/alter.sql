cfae9d85a49908617c150fd540266d8989ad53fb
jzw <zhengwei.jin@gmail.com>
2013-10-01 08:26:05 +0800

    在历史表中添加操作字段

--- server/build/com.zizibujuan.drip.server.sql/mysql/alter.sql
+++ server/build/com.zizibujuan.drip.server.sql/mysql/alter.sql
@@ -8,3 +8,5 @@ alter table DRIP_USER_INFO add column `FOLLOW_COUNT` INT NULL DEFAULT 0 COMMENT 
 alter table DRIP_USER_INFO add column `EXER_DRAFT_COUNT` INT NULL DEFAULT 0 COMMENT '录入的习题草稿数量';
 alter table DRIP_USER_INFO add column `EXER_PUBLISH_COUNT` INT NULL DEFAULT 0 COMMENT '录入的已发布习题数量';
 alter table DRIP_USER_INFO add column `ANSWER_COUNT` INT NULL DEFAULT 0 COMMENT '回答的习题数量';
+
+alter table DRIP_EXERCISE change column `EXER_CATEGORY` `EXER_COURSE` CHAR(3);
