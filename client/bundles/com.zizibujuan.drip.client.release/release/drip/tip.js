4095ac6ef35a7d5c636b6d8e550d62de800c0c3b
jzw <zhengwei.jin@gmail.com>
2013-11-23 07:42:32 +0800

    build client

--- client/bundles/com.zizibujuan.drip.client.release/release/drip/tip.js
+++ client/bundles/com.zizibujuan.drip.client.release/release/drip/tip.js
@@ -1,3 +1,3 @@
 //>>built
-define("drip/tip",["dojo/_base/window","dojo/fx","dojo/dom-construct"],function(g,c,a){return{ok:function(d,e,f){var b=a.create("span",{style:{color:"green"}},e,f);b.innerHTML="\x3ci class\x3d'icon-ok'\x3e\x3c/i\x3e "+d;c.wipeOut({node:b,duration:1E3,onEnd:function(){a.destroy(b)}}).play()},error:function(a){}}});
+define("drip/tip",["dojo/_base/window","dojo/fx","dojo/dom-construct"],function(g,f,a){return{ok:function(b,c,d){var e=a.create("span",{style:{color:"green"}},c,d);e.innerHTML="\x3ci class\x3d'icon-ok'\x3e\x3c/i\x3e "+b;f.wipeOut({node:e,duration:1E3,onEnd:function(){a.destroy(e)}}).play()},error:function(b,c,d){email.nextSibling&&a.destroy(email.nextSibling);a.create("span",{style:{color:"red"}},c,d).innerHTML="\x3ci class\x3d'icon-remove-sign'\x3e\x3c/i\x3e "+b}}});
 //@ sourceMappingURL=tip.js.map
