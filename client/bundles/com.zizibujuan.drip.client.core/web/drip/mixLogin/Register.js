f217bb48dbe7f52f14827df871a4c2c3f15bfb2e
jzw <zhengwei.jin@gmail.com>
2014-02-23 08:43:30 +0800

    注册用户时，修改注册按钮的文本并让注册按钮失效，同时将注册控件中的文本国际化

--- client/bundles/com.zizibujuan.drip.client.core/web/drip/mixLogin/Register.js
+++ client/bundles/com.zizibujuan.drip.client.core/web/drip/mixLogin/Register.js
@@ -4,22 +4,28 @@ define(["dojo/_base/declare",
         "dojo/on",
         "dojo/keys",
         "dojo/string",
+        "dojo/dom-class",
         "dojo/dom-form",
         "dojo/dom-construct",
+        "dojo/dom-attr",
         "dijit/_WidgetBase",
         "dijit/_TemplatedMixin",
-        "dojo/text!./templates/Register.html"], function(
+        "dojo/text!./templates/Register.html",
+        "dojo/i18n!./nls/Register"], function(
         		declare,
         		lang,
         		xhr,
         		on,
         		keys,
         		string,
+        		domClass,
         		domForm,
         		domConstruct,
+        		domAttr,
         		_WidgetBase,
         		_TemplatedMixin,
-        		registerTemplate){
+        		registerTemplate,
+        		RegisterMessages){
 	
 	return declare("drip.mixLogin.Register", [_WidgetBase, _TemplatedMixin], {
 		
@@ -29,6 +35,12 @@ define(["dojo/_base/declare",
 		
 		postCreate: function(){
 			this.inherited(arguments);
+			
+			domAttr.set(this.email, "placeholder", RegisterMessages.placeholder_email);
+			domAttr.set(this.password, "placeholder", RegisterMessages.placeholder_pwd);
+			domAttr.set(this.loginName, "placeholder", RegisterMessages.placeholder_login_name);
+			
+			domAttr.set(this.btnSignup, "innerHTML", RegisterMessages.label_btn_register);
 			
 			on(this.btnSignup, "click", lang.hitch(this, this._signup));
 			
@@ -61,6 +73,10 @@ define(["dojo/_base/declare",
 				return;
 			}
 			console.log("signup");
+			domAttr.set(this.btnSignup, "disabled", true);
+			domClass.add(this.btnSignup, "submit_tip");
+			this.btnSignup.innerHTML = RegisterMessages.label_btn_registering;
+			
 			var data = domForm.toJson(this.signupForm);
 			xhr.post("/users", {
 				handleAs: "json", 
@@ -76,32 +92,32 @@ define(["dojo/_base/declare",
 			
 			email = string.trim(email);
 			if(email == ""){
-				errors.push("请输入常用邮箱");
+				errors.push(RegisterMessages.tip_email_required);
 			}else if(email.length > 50){
-				errors.push("邮箱不能超过50个字符");
+				errors.push(RegisterMessages.tip_email_max_length);
 			}else if(!/[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/.test(email)){
-				errors.push("邮箱格式不正确");
+				errors.push(RegisterMessages.tip_email_invalid);
 			}
 			
 			if(password == ""){
-				errors.push("请输入密码");
+				errors.push(RegisterMessages.tip_pwd_required);
 			}else if(password.length > 0 && string.trim(password) == ""){
-				errors.push("密码不能全为空格");
+				errors.push(RegisterMessages.tip_pwd_not_blank);
 			}else if(password.length < 6 || password.length > 20){
-				errors.push("密码长度应为6到20个字符");
+				errors.push(RegisterMessages.tip_pwd_length);
 			}else if(/^[0-9]+$/.test(password)){
-				errors.push("密码不能全为数字");
+				errors.push(RegisterMessages.tip_pwd_not_all_number);
 			}else if(/^[A-Za-z]+$/.test(password)){
-				errors.push("密码不能全为字母");
+				errors.push(RegisterMessages.tip_pwd_not_all_letter);
 			}
 			
 			loginName = string.trim(loginName);
 			if(loginName == ""){
-				errors.push("请输入用户名");
+				errors.push(RegisterMessages.tip_login_name_required);
 			}else if(loginName.length > 20){// 无需计算中文字符，因为下面限制为不能输入中文字符
-				errors.push("用户名不能多于20个字符")
+				errors.push(RegisterMessages.tip_login_name_length)
 			}else if(!/^(?![-_])[a-zA-Z0-9_-]+$/.test(loginName)){
-				errors.push("用户名只能包含英文字母，数字,-或_，不能以-或_开头，不区分大小写");
+				errors.push(RegisterMessages.tip_login_name_invalid);
 			}
 			
 			this.errors = errors;
@@ -128,6 +144,10 @@ define(["dojo/_base/declare",
 		},
 		
 		_signupError: function(error){
+			domAttr.set(this.btnSignup, "disabled", false);
+			this.btnSignup.innerHTML = RegisterMessages.label_btn_register;
+			domClass.remove(this.btnSignup, "submit_tip");
+			
 			if(error.response.data){
 				this.errors = error.response.data;
 				this._showErrors();
---
+++ client/bundles/com.zizibujuan.drip.client.core/web/drip/mixLogin/nls/Register.js
@@ -0,0 +1,27 @@
+define({
+	root:({
+		label_btn_register: "注册",
+		label_btn_registering: "注册中…",
+		placeholder_email: "邮箱",
+		placeholder_pwd: "密码",
+		placeholder_login_name: "用户名",
+		
+		tip_email_required: "请输入常用邮箱",
+		tip_email_max_length: "邮箱不能超过50个字符",
+		tip_email_invalid: "邮箱格式不正确",
+		
+		tip_pwd_required: "请输入密码",
+		tip_pwd_not_blank: "密码不能全为空格",
+		tip_pwd_length: "密码长度应为6到20个字符",
+		tip_pwd_not_all_number: "密码不能全为数字",
+		tip_pwd_not_all_letter: "密码不能全为字母",
+		
+		tip_login_name_required: "请输入用户名",
+		tip_login_name_length: "用户名不能多于20个字符",
+		tip_login_name_invalid: "用户名只能包含英文字母，数字,-或_，不能以-或_开头，不区分大小写"
+		
+		
+	}),
+
+	zh : true
+});
